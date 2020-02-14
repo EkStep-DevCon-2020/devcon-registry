8b0170c76c6cba23a09ec7bb6a468b11e367d49c
satishkumar.nirmalkar@tarento.com <satishkumar.nirmalkar@tarento.com>
2020-02-14 14:26:40 +0530

    adding change

--- devcon-util-service/index.js
+++ devcon-util-service/index.js
@@ -4,6 +4,7 @@ const app = express();
 var bodyParser = require("body-parser");
 var cors = require("cors")
 const request = require('request')
+var cacheManager = require('cache-manager');
 
 const server = http.createServer(app);
 var baseUrl = "http://localhost:8080";
@@ -13,14 +14,18 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 
-app.post("/add", (req,response,callback)=>{
+var cache = require('./cache.js');
+cache.set('id', 0)
+
+//Add Stall Details
+app.post("/stall/add", (req,response,callback)=>{
 
   
     var url = baseUrl + "/add"
 
     //Get Visitor Details
 
-    let option1 = {
+    let option = {
         json: true,
         headers: {
             "Content-Type":"application/json"
@@ -28,7 +33,7 @@ app.post("/add", (req,response,callback)=>{
         body: req.body,
         url: url
     }
-    request.post(option1, function (err, res) {
+    request.post(option, function (err, res) {
         if (res) {
                 response.send(res.body)
         }else{
@@ -37,15 +42,14 @@ app.post("/add", (req,response,callback)=>{
 })
 })
 
-
-app.post("/read", (req,response,callback)=>{
+app.post("/stall/get", (req,response,callback)=>{
 
   
-    var url = baseUrl + "/read"
+    var url = baseUrl + "/search"
 
-    //Get Visitor Details
+    //Get Stall Details
 
-    let option1 = {
+    let option = {
         json: true,
         headers: {
             "Content-Type":"application/json"
@@ -53,15 +57,99 @@ app.post("/read", (req,response,callback)=>{
         body: req.body,
         url: url
     }
-    request.post(option1, function (err, res) {
+    request.post(option, function (err, res) {
         if (res) {
                 response.send(res.body)
         }else{
             console.log(err)
         }
 })
-}
-)
+})
+
+app.post("/ideas/get", (req,response,callback)=>{
+
+  
+    var url = baseUrl + "/search"
+
+    //Get Idea Details
+
+    let option = {
+        json: true,
+        headers: {
+            "Content-Type":"application/json"
+        },
+        body: req.body,
+        url: url
+    }
+    request.post(option, function (err, res) {
+        if (res) {
+               var ideaDetails = res.body
+                var entityArray = req.body.request.entityType
+                entityArray.pop()
+                entityArray.push("Vote")
+
+                let option2 = {
+                    json: true,
+                    headers: {
+                        "Content-Type":"application/json"
+                    },
+                    body:req.body,
+                    url: baseUrl+"/search"
+                    
+                 }
+
+                 request.post(option2, function (err, resp) {
+                    if(resp){
+                        var voteDetails =  resp.body
+
+                        ideaDetails.result.Idea.forEach(idea => {
+                            var totalPoints = 0
+                            voteDetails.result.Vote.forEach(vote => {
+                                var point = vote.points
+                                if(point == undefined){
+                                    point = 0;
+                                }
+                                totalPoints +=point
+                            });
+                            idea['totalPoints'] = totalPoints
+                        });
+
+                        response.send(ideaDetails)
+
+                    }
+                })
+        }else{
+            console.log(err)
+        }
+})
+})
+
+
+
+// app.post("/read", (req,response,callback)=>{
+
+  
+//     var url = baseUrl + "/read"
+
+//     //Get Visitor Details
+
+//     let option1 = {
+//         json: true,
+//         headers: {
+//             "Content-Type":"application/json"
+//         },
+//         body: req.body,
+//         url: url
+//     }
+//     request.post(option1, function (err, res) {
+//         if (res) {
+//                 response.send(res.body)
+//         }else{
+//             console.log(err)
+//         }
+// })
+// }
+// )
 
 app.post("/search", (req,response,callback)=>{
 
@@ -81,6 +169,7 @@ app.post("/search", (req,response,callback)=>{
     request.post(option1, function (err, res) {
         if (res) {
                 response.send(res.body)
+
         }else{
             console.log(err)
         }
@@ -88,32 +177,168 @@ app.post("/search", (req,response,callback)=>{
 })
 
 
-app.post("/update", (req,response,callback)=>{
+// app.post("/update", (req,response,callback)=>{
 
   
-    var url = baseUrl + "/update"
+//     var url = baseUrl + "/update"
 
-    //Get Visitor Details
+//     //Get Visitor Details
 
-    let option1 = {
+//     let option1 = {
+//         json: true,
+//         headers: {
+//             "Content-Type":"application/json"
+//         },
+//         body: req.body,
+//         url: url
+//     }
+//     request.post(option1, function (err, res) {
+//         if (res) {
+//                 response.send(res.body)
+//         }else{
+//             console.log(err)
+//         }
+//     })
+// })
+
+app.post("/visitor/defaultAdd", (req,response,callback)=>{
+    let option = {
         json: true,
         headers: {
             "Content-Type":"application/json"
         },
         body: req.body,
-        url: url
+        url: baseUrl+"/add"
     }
-    request.post(option1, function (err, res) {
-        if (res) {
-                response.send(res.body)
-        }else{
-            console.log(err)
+
+    request.post(option, function (err, res) {
+
+        if(res){
+            response.send(res.body)
         }
     })
+
 })
 
 
-app.post("/getVisitor", (req,response,callback)=>{
+app.post("/visitor/add", (req,response,callback)=>{
+
+  
+
+    var visitorCode = req.body.request.Visitor.code
+    isCreate = false
+    if(visitorCode == undefined){
+        isCreate = true
+    }
+
+    if(isCreate){
+        var id =cache.get('id'); 
+        id = id +1
+        cache.set('id', id);
+        visitorCode = "VID"+id
+
+        req.body.request.Visitor['code']=visitorCode
+        let option1 = {
+            json: true,
+            headers: {
+                "Content-Type":"application/json"
+            },
+            body: req.body,
+            url: baseUrl+"/add"
+        }
+
+        request.post(option1, function (err, res) {
+
+            if(res){
+
+                var visitorReponse = res.body
+                visitorReponse.result.Visitor['code']=visitorCode
+                response.send(visitorReponse)
+            }
+        })
+
+
+    }else{
+
+        // Vistor Details 
+        var searchTemplate ={
+            id: "open-saber.registry.search",
+            ver: "1.0",
+            ets: "11234",
+            params: {
+                did: "",
+                key: "",
+                msgid: ""
+            },
+         request: {
+           entityType:["Visitor"],
+           filters:{
+               code:{
+                   eq:visitorCode
+               }
+           }           
+    
+         }
+        }
+        let option1 = {
+            json: true,
+            headers: {
+                "Content-Type":"application/json"
+            },
+            body: searchTemplate,
+            url: baseUrl+"/search"
+        }
+        request.post(option1, function (err, res) {
+            if (res) {
+
+                   var visitorDetails = res.body
+                   var visOsid = visitorDetails.result.Visitor[0].osid
+
+                   var updateTemplate ={
+                    id: "open-saber.registry.update",
+                    ver: "1.0",
+                    ets: "11234",
+                    params: {
+                        did: "",
+                        key: "",
+                        msgid: ""
+                    },
+                 request: {
+                    Visitor:{
+                        code:visitorCode,
+                        name: req.body.request.Visitor.name 
+
+                    }
+                 }           
+            
+                }
+                
+
+                   updateTemplate.request.Visitor['osid']=visOsid
+                   let option1 = {
+                    json: true,
+                    headers: {
+                        "Content-Type":"application/json"
+                    },
+                    body: updateTemplate,
+                    url: baseUrl+"/update"
+                   }
+                   request.post(option1, function (err, resp) {
+                    if (resp) {
+                            response.send(resp.body)
+                    }
+                
+                })
+                
+
+            }
+        })
+             
+    }
+})
+
+
+app.post("/visitor/detail/get", (req,response,callback)=>{
 
     var readSuffix = "read"
     var url = baseUrl + "/" + readSuffix
@@ -222,7 +447,7 @@ app.post("/getVisitor", (req,response,callback)=>{
                     if(val==undefined){
                         val = 0;
                     }
-                    totalPoints = val;
+                    totalPoints += val;
                 });
                 
                 if(voteDetails.result.Vote == undefined){
@@ -263,7 +488,7 @@ app.post("/getVisitor", (req,response,callback)=>{
 
 
 
-app.post("/getAllVisitor", (req,response,callback)=>{
+app.post("/visitor/detail/getall", (req,response,callback)=>{
 
     
     var searchTemplate ={
@@ -352,7 +577,7 @@ app.post("/getAllVisitor", (req,response,callback)=>{
                                     if(val==undefined){
                                         val = 0;
                                     }
-                                    totalPoints = val;
+                                    totalPoints += val;
                                 });
                                 
                                 visiters.forEach(visitor => {
